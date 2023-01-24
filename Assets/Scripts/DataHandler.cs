e2f0631b962b38b86532308ed7b00972bfdbed63
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-01-24 19:29:40 +0545

    Code Refactored and UI changed
    - Unnecessary codes removed
    - Button images modified
    - Button placement locations changed
    - Previous furniture placement method changed

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -2,24 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using UnityEngine.AddressableAssets;
-//using System.Threading.Tasks;
 
 public class DataHandler : MonoBehaviour
 {
     private GameObject furniture;
     [SerializeField]
-    private ButtonManager buttonPrefab;
-    [SerializeField]
-    private GameObject buttonContainer;
-    [SerializeField]
     private List<Item> items;
     public Text textElement;
     public string ReceivedMessagFromAndroid;
-    //[SerializeField]
-    // private string label;
-
-    private int curretn_id = 0;
 
     private static DataHandler instance;
     public static DataHandler Instance
@@ -43,8 +33,6 @@ public class DataHandler : MonoBehaviour
     private void Awake()
     {
         LoadItems();
-        // await Get(label);
-        CreateButton();
     }
 
     void LoadItems()
@@ -56,36 +44,9 @@ public class DataHandler : MonoBehaviour
         }
     }
 
-
-    void CreateButton()
-    {
-        foreach (Item i in items)
-        {
-            ButtonManager bm = Instantiate(buttonPrefab, buttonContainer.transform);
-            bm.ItemId = curretn_id;
-            bm.ButtonTexture = i.itemImage;
-            curretn_id++;
-        }
-    }
-
-    public void SetFurniture(int id)
-    {
-        furniture = items[id].itemPrefab;
-    }
-
     public GameObject GetFurniture()
     {
+        furniture = items[0].itemPrefab;
         return furniture;
     }
-
-    /*  public async Task Get(string label)
-      {
-          var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
-          foreach(var location in locations)
-          {
-              var obj = await Addressables.LoadAssetAsync<Item>(location).Task;
-              items.Add(obj);
-          }
-      }
-    */
 }
