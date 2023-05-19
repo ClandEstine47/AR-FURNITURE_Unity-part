9d5c67a6d3456f57801f539097c9b4a338564ce3
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-05-19 17:59:26 +0545

    Screenshot Functionality Added
    Screenshot feature added.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.Interaction.Toolkit.AR;
 using UnityEngine.UI;
+using System.IO;
+using System.Collections;
 
 public class InputManager : ARBaseGestureInteractable
 {
@@ -23,6 +25,11 @@ public class InputManager : ARBaseGestureInteractable
     public Button placeFurnitureBtn;
     [SerializeField]
     public Button backBtn;
+    [SerializeField]
+    public Button ScreenshotBtn;
+
+    public GameObject UI;
+    public GameObject CrossHair;
 
     // Start is called before the first frame update
     void Start()
@@ -55,6 +62,30 @@ public class InputManager : ARBaseGestureInteractable
     public void QuitUnity()
     {
         Application.Quit();
+    }
+
+    public void TakeScreenshot()
+    {
+        UI.SetActive(false);
+        CrossHair.SetActive(false);
+        StartCoroutine("Screenshot");
+    }
+
+    private IEnumerator Screenshot()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        texture.Apply();
+
+        string name = "Screenshot_ARFurniture" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+
+        NativeGallery.SaveImageToGallery(texture, "AR Furniture", name);
+
+        Destroy(texture);
+        UI.SetActive(true);
+        CrossHair.SetActive(true);
     }
 
     // Update is called once per frame
